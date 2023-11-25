0cd328531098bc6430899d1c6941dd0ec7b7be48
KhawlahHad <148780073+KhawlahHad@users.noreply.github.com>
2023-11-25 20:21:33 +0300

    Swagger

--- main.py
+++ main.py
@@ -32,33 +32,31 @@ app = FastAPI()
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
-@router.get("/", response_class=HTMLResponse)
-def read_root(request: Request):
-    return templates.TemplateResponse("welcome.html",
-                                    {"request": request}
-                                    )
-
-@router.get("/predict")
-async def predict_html(request: Request):
-    return templates.TemplateResponse("form.html",
-                                    {"request": request}
-                                    )
-
-@router.post("/predict", response_class=HTMLResponse)
-async def predict(request: Request, N: int = Form(...), P: int = Form(...), K: int = Form(...), ph: float = Form(...)):
-    soil = Soil(N=N, P=P, K=K, ph=ph) 
-    return templates.TemplateResponse(
-        "result.html",
-        {"request": request, "crop": await model(soil)},
-    )
-
-@router.post("/api/predict")
+@router.post("/api/predict", tags=["API"], summary="Predict Function")
 async def predict(soil: Soil):
     prediction = await model(soil)
     return {
         "crop": prediction
     }
 
+@router.get("/", response_class=HTMLResponse, tags=["WebApp"], summary="Home Page")
+def read_root(request: Request):
+    return templates.TemplateResponse("welcome.html",
+                                    {"request": request}
+                                    )
 
+@router.get("/predict", tags=["WebApp"], summary="Predict Page")
+async def predict_html(request: Request):
+    return templates.TemplateResponse("form.html",
+                                    {"request": request}
+                                    )
+
+@router.post("/predict", response_class=HTMLResponse, tags=["WebApp"], summary="Predict Page")
+async def predict(request: Request, N: int = Form(...), P: int = Form(...), K: int = Form(...), ph: float = Form(...)):
+    soil = Soil(N=N, P=P, K=K, ph=ph) 
+    return templates.TemplateResponse(
+        "result.html",
+        {"request": request, "crop": await model(soil)},
+    )
 
 app.include_router(router)
